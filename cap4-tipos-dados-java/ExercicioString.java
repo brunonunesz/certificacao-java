260b7c913c40679946c7f8e7aa8d403592f01908
brunonunesz <brunonunesz@outlook.com>
2019-02-08 21:16:05 -0200

    Capitulo 12 - done

--- cap4-tipos-dados-java/ExercicioString.java
+++ cap4-tipos-dados-java/ExercicioString.java
@@ -1,7 +1,7 @@
 public class ExercicioString{
     public static void main(String[] args){
-        String s = "estudando para a certificação";
-        System.out.println(s.substring(3, 6));
+        String s = new String();
+        
     }
 }
 
